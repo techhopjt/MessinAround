3aaf093c53b7b7992c7fcd9f8b243e1b9e3c2d4c
Julian Torres <techhop.jt@gmail.com>
2014-04-02 10:42:08 -0400

    Update 2

--- My Projects/DateList/DateList/main.m
+++ My Projects/DateList/DateList/main.m
@@ -12,10 +12,29 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        // Create three NSDate objects
+        NSDate *now = [NSDate date];
+        NSDate *tomorrow = [now dateByAddingTimeInterval:24.0*60.0*60.0];
+        NSDate *yesterday = [now dateByAddingTimeInterval:-24.0*60.0*60.0];
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // Create an array containing all three
+        NSMutableArray *dateList = [NSMutableArray array];
         
+        // Add two dates to the array
+        [dateList addObject:now];
+        [dateList addObject:tomorrow];
+        
+        // Add yesterday at the beginning of the list
+        [dateList insertObject:yesterday atIndex:0];
+        
+        // Iterate over the array
+        for (NSDate *d in dateList) {
+            NSLog(@"Here is a date: %@", d);
+        }
+        
+        // Remove yesterday
+        [dateList removeObjectAtIndex:0];
+        NSLog(@"Now the first date is %@", dateList[0]);
     }
     return 0;
 }
