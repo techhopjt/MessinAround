bd89e6c238fadc4e60f80948b7a235b55ba373f8
Julian Torres <techhop.jt@gmail.com>
2014-04-01 21:48:39 -0400

    Initial Commit

---
+++ My Projects/DateList/DateList/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DateList
+//
+//  Created by Julian Torres on 4/1/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
